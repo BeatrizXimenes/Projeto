7dece062decb29b1811273f138f94496ac71b507
BeatrizXimenes <xximenes8@gmail.com>
2022-09-06 23:25:58 -0300

    first commit

---
+++ Projetoo.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Olá mundo isso é um teste</h1>
+
+    <fieldset >
+        <form method="get" action="adiciona--produto.php"> 
+            
+            Nome: <input type ="text" name="nome" id="nome" value="">
+            Sobrenome: <input type="text" name="sobrenome" id="sobre" value="">
+            Email:<input type = "email" name="email" id="email" value="">
+            Senha:<input type="text"  name="senha" id="senha" value="">
+            <input type="submit" value="Cadastrar" />
+
+
+        </form>
+    </fieldset>
+
+
+    <fieldset>
+        
+    
+
+    </fieldset>
+    
+</body>
+</html>
